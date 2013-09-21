6f5f93ca6718aaf14ce0c427ff7597f041c40501
Thomas Hebb <tommyhebb@gmail.com>
2013-09-21 16:45:12 -0400

    Reorder simple-flash example so the function calls match the description

--- docs/samples/simple-flash/imager.sh
+++ docs/samples/simple-flash/imager.sh
@@ -4,5 +4,5 @@
 # contain an Android boot image (prepared using cc-mangle-bootimg) and a
 # squashfs root filesystem, respectively.
 
-flash_mtd_partition 'rootfs' 'system.img'
 flash_mtd_partition 'kernel' 'boot.img'
+flash_mtd_partition 'rootfs' 'system.img'
