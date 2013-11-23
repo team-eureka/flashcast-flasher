b901e7ec7bba0eae41138482296871950f716c45
riptidewave93 <chrisrblake93@gmail.com>
2013-11-23 10:53:05 -0600

    Fixes requested in Pull Request

--- bundled-mods/full-restore/imager.sh
+++ bundled-mods/full-restore/imager.sh
@@ -1,16 +1,13 @@
 set -e
 
 if test -z "$1" ; then
-        fatal "No Backup Folder Defined in full_backup"
+        fatal "No Backup Folder Defined in full_restore"
 fi
 
 # $1 should be the folder number of your backup, ex 1 for ./backup-1
 # it should NOT be a full path!
 
 restore_mtd_partition 'rootfs' "$1"
-
 restore_mtd_partition 'kernel' "$1"
-
 restore_mtd_partition 'recovery' "$1"
-
 restore_mtd_partition 'userdata' "$1"
