00460a58315e7e125e673daddc31ea2e3cc2aeec
riptidewave93 <chris@0x40.co>
2015-10-25 07:50:20 -0500

    Add support for CustomDNS and Startup Scripts
    Let's get this into the new baby, as I have a feeling these will be
    requested.

--- bundled-mods/modified-ota/imager.sh
+++ bundled-mods/modified-ota/imager.sh
@@ -27,12 +27,21 @@ rm "${ROOTFS}/boot/recovery.img"
 dd if=/dev/mtdblock6 of="${ROOTFS}/boot/recovery.img" #Dirty but functional
 
 log "Adding Busybox"
-cp "./busybox" "${ROOTFS}/bin/"
+cp "./files/busybox" "${ROOTFS}/bin/"
+chmod +x "${ROOTFS}/bin/busybox"
 
 log "Enabling Telnet Access"
 rm "${ROOTFS}/bin/sntpd"
-echo -e '#!/bin/sh\n/system/bin/busybox telnetd -l /system/bin/sh\n/bin/toolbox sntpd' > "${ROOTFS}/bin/sntpd"
+echo -e '#!/bin/sh\n/system/bin/busybox telnetd -l /system/bin/sh\n' > "${ROOTFS}/bin/sntpd"
+
+log "Enabling Startup Script Support"
+echo -e 'if busybox test -f "/data/user_boot_script.sh"; then\n    /data/user_boot_script.sh &\nfi\n\n/bin/toolbox sntpd' >> "${ROOTFS}/bin/sntpd"
 chmod +x "${ROOTFS}/bin/sntpd"
+
+log "Enabling Custom DNS Server Support"
+rm "${ROOTFS}/etc/dhcpcd/dhcpcd-hooks/20-dns.conf"
+cp "./files/20-dns.conf" "${ROOTFS}/etc/dhcpcd/dhcpcd-hooks/20-dns.conf"
+chmod 700 "${ROOTFS}/etc/dhcpcd/dhcpcd-hooks/20-dns.conf"
 
 log "Done Editing, Writing Changes"
 end_squashfs_edit "$ROOTFS"
